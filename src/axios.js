fbbd1f78e32a6bd5666e93ff0b124bf1fe34ddd2
ViAsmit <asmit.vml.962@gmail.com>
2021-04-28 09:23:38 +0530

    heroku

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/api/";
+// const baseURL = "http://127.0.0.1:8000/api/";
+const baseURL = "https://covid-19-oxygen.herokuapp.com/api/";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
